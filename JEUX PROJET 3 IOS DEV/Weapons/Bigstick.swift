e54ae32282c25326fd8a80745b634f0d45b9605b
lorenzolau <lorenzolau@hotmail.com>
2018-10-18 12:28:28 +0200

    adding new weapons class

---
+++ JEUX PROJET 3 IOS DEV/Weapons/Bigstick.swift
@@ -0,0 +1,17 @@
+//
+//  Bigstick.swift
+//  JEUX PROJET 3 IOS DEV
+//
+//  Created by Pyran on 18/10/2018.
+//  Copyright © 2018 Spirox. All rights reserved.
+//
+
+import Foundation
+
+class Bigstick: Weapon{
+    
+    init(){
+        super.init(damage: 0, heal: 50)
+    }
+    
+}
---
+++ JEUX PROJET 3 IOS DEV/Weapons/Mysticweapon.swift
@@ -0,0 +1,17 @@
+//
+//  Mysticweapon.swift
+//  JEUX PROJET 3 IOS DEV
+//
+//  Created by Pyran on 18/10/2018.
+//  Copyright © 2018 Spirox. All rights reserved.
+//
+
+import Foundation
+
+class Mysticweapon: Weapon{
+    
+    init(){
+        super.init(damage: 50, heal: 0)
+    }
+    
+}
